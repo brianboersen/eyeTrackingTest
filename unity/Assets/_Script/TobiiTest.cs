60cea2e5104129044d08fed5a3cadbd23b3c2b25
dude with commits <No>
2018-01-23 13:29:38 +0100

    first tests

---
+++ unity/Assets/_Script/TobiiTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class TobiiTest : MonoBehaviour
+{
+       
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        GameObject focusedObject = TobiiAPI.GetFocusedObject();
+        if (null != focusedObject)
+        {
+            print("The focused game object is: " + focusedObject.name + " (ID: " + focusedObject.GetInstanceID() + ")");
+        }
+    }
+}
